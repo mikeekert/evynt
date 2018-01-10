c19318fa016954ebd36643b8a1ee67e1177c8c5e
Mike Ekert <mikeekert@gmail.com>
2018-01-10 15:10:19 -0600

    scss configured. incoming feed mapped to array

--- evynt/src/app/feed/feed.component.ts
+++ evynt/src/app/feed/feed.component.ts
@@ -1,41 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, Headers } from "@angular/http";
-import { AuthHttp } from 'angular2-jwt';
-import { environment } from "../../environments/environment";
+import {Component, OnInit} from '@angular/core';
+import {Http, Headers} from "@angular/http";
+import {AuthHttp} from 'angular2-jwt';
+import {environment} from "../../environments/environment";
 
-@Component({
-  selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
-})
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+
+import {FeedItem} from './feed-item';
+
+@Component({selector: 'app-feed', templateUrl: './feed.component.html', styleUrls: ['./feed.component.scss']})
 export class FeedComponent implements OnInit {
-  feed: string;
+  feed : FeedItem[];
 
   private feedApiUrn = "api/Post/ByUser/xn4FRYDxwoeXiasmjC6S822xjeU2"; //TODO: update this to /api/Post/Latitude/{latitude}/Longitude/{longitude}/Range/{range}/Minimum/{minimum}
-  private apiUriBase: string;
-  
-  constructor(private http: AuthHttp) { 
+  private apiUriBase : string;
+
+  constructor(private http : AuthHttp) {
     this.apiUriBase = environment.citiskopeApi.baseUrl;
   }
 
   ngOnInit() {
     //Gets posts
-    this.getFeed().subscribe(response => {
-      this.feed = response.text(); //TODO: make objects to return to the view
-    });
+    this
+      .getFeed()
+      .subscribe(data => {
+        this.feed = data; //TODO: make objects to return to the view
+        console.log(this.feed);
+      })
   }
 
   getFeed() {
     const apiUrl = `${this.apiUriBase}/${this.feedApiUrn}`;
-    return this.http.get(
-            apiUrl,
-            {
-                headers: new Headers({
-                    "Accept": "application/json",
-                    "Content-Type": "application/json" 
-                })
-            }
-        )
+    return this
+      .http
+      .get(apiUrl, {
+        headers: new Headers({"Accept": "application/json", "Content-Type": "application/json"})
+      })
+      .map(res => res.json().data)
   }
 
 }
