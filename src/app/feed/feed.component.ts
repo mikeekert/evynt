1d4f0f4f4539ccacfd2b6a6cba7c165a8c1e829d
Mike Ekert <mikeekert@gmail.com>
2018-02-07 19:07:48 -0600

    changed dir structure. added modules for Feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -11,10 +11,13 @@ import {FeedItem} from './feed-item';
 @Component({selector: 'app-feed', templateUrl: './feed.component.html', styleUrls: ['./feed.component.scss']})
 export class FeedComponent implements OnInit {
   feed : FeedItem[];
-  location : {};
+  location : any;
 
-  private feedApiUrn = "api/Post/ByUser/xn4FRYDxwoeXiasmjC6S822xjeU2"; //TODO: update this to /api/Post/Latitude/{latitude}/Longitude/{longitude}/Range/{range}/Minimum/{minimum}
+  // TODO: update this to
+  // /api/Post/Latitude/{latitude}/Longitude/{longitude}/PageSize/{pagesize}/Page/
+  // { page}
   private apiUriBase : string;
+  private feedApiUrn : string;
 
   constructor(private http : AuthHttp) {
     this.apiUriBase = environment.citiskopeApi.baseUrl;
@@ -27,18 +30,18 @@ export class FeedComponent implements OnInit {
       .getPosition()
       .then((position) => {
         this.location = position;
+        console.log(this.location.coords)
+
+        this
+          .getFeed(this.location.coords.longitude, this.location.coords.latitude)
+          .subscribe(data => {
+            this.feed = data; //TODO: make objects to return to the view
+            console.log(this.feed);
+          })
       })
       .catch((err) => {
         // TODO: use error handling for incompatible browsers
       });
-
-    //Gets posts
-    this
-      .getFeed()
-      .subscribe(data => {
-        this.feed = data; //TODO: make objects to return to the view
-        console.log(this.feed);
-      })
   }
 
   getPosition() {
@@ -49,12 +52,14 @@ export class FeedComponent implements OnInit {
     });
   }
 
-  getFeed() {
+  getFeed(lat, long) {
+    this.getPosition();
+    this.feedApiUrn = "api/Post/Latitude/" + lat + "/Longitude/" + long + "/PageSize/10/Page/1";
     const apiUrl = `${this.apiUriBase}/${this.feedApiUrn}`;
     return this
       .http
       .get(apiUrl, {
-        headers: new Headers({"Accept": "application/json", "Content-Type": "application/json"})
+        headers: new Headers({"Accept": "application/json", "Content-Type": "application/json", "Access-Control-Allow-Origin":"*"})
       })
       .map(res => res.json().data)
   }
