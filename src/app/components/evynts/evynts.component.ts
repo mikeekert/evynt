6520a119a5166655055e2d5a89d2c11aaba46b5c
Karl Merecido <kmerecido@gmail.com>
2018-03-18 00:17:25 -0500

    added cards with infinate scroll

--- src/app/components/evynts/evynts.component.ts
+++ src/app/components/evynts/evynts.component.ts
@@ -10,11 +10,22 @@ import {Evynt} from "../../models/evynt";
 })
 export class EvyntsComponent implements OnInit {
   Evynts: Evynt[] = [];
+  private pageSize = 10;
+  private page = 1;
   constructor(private evyntService: EvyntService) { }
 
   ngOnInit() {
+    this.getEvynts(this.pageSize, this.page)
+  }
+
+  onScroll(){
+    this.page = ++this.page;
+    this.getEvynts(this.pageSize, this.page)
+  }
+
+  private getEvynts(pageSize: number,  page: number){
     this.evyntService
-      .get(10,1) //TODO: do something with scrolling to affect paging.
+      .get(pageSize, page)
       .subscribe(data => {
         data.forEach((item) =>{
           this
