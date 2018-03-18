158567a0e964c71e9d74aae88f9ce3a09c10f660
Mike Ekert <mike.ekert@totalexpert.com>
2018-03-18 11:44:14 -0500

    tslint

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,30 +3,30 @@ import {EvyntService} from '../../services/evynt.service';
 import {Carousel, ApiEvyntResponse} from './carousel/carousel';
 import {Subject} from 'rxjs/Subject';
 import {ISubscription} from 'rxjs/Subscription';
-import {Post} from "../../models/post";
-import {PostService} from "../../services/post.service";
+import {Post} from '../../models/post';
+import {PostService} from '../../services/post.service';
 
 @Component({selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.scss']})
 export class HomeComponent implements OnInit {
   CarouselFeed: Carousel[] = [];
-  Posts: Post[] = []
+  Posts: Post[] = [];
   constructor(private evyntService: EvyntService, private postService: PostService) {}
 
   ngOnInit() {
     this.evyntService
       .getFeatured()
       .subscribe(data => {
-        data.forEach(item =>{
+        data.forEach(item => {
           this.CarouselFeed.push(new Carousel(item));
           });
       });
 
     this.postService
       .get(10, 1)
-      .subscribe( data =>{
-        data.forEach(item =>{
+      .subscribe( data => {
+        data.forEach(item => {
           this.Posts.push(new Post(item));
-        })
-      })
+        });
+      });
   }
 }
