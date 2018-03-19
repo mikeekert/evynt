954107e142efe64fea9d0d56c5def6bc86e9d66d
Mike Ekert <mike.ekert@totalexpert.com>
2018-03-18 22:59:44 -0500

    fixed ngfor

--- src/app/components/flyer/flyer.component.ts
+++ src/app/components/flyer/flyer.component.ts
@@ -4,7 +4,7 @@ import {Flyer} from '../../models/flyer';
 @Component({selector: 'app-flyer', templateUrl: './flyer.component.html', styleUrls: ['./flyer.component.scss']})
 export class FlyerComponent implements OnInit {
 
-  @Input()Flyers: Flyer;
+  @Input()flyer: Flyer;
 
   constructor() {}
 
