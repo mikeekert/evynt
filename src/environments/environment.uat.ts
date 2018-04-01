d881dfb43b991535102d3862570dfa93c6092b71
Karl Merecido <kmerecido@gmail.com>
2018-04-01 12:36:10 -0500

    added uat environment

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCDCwEEiyxCBHTuC-7EDq0ua-RlLCKrVKA",
+    authDomain: "citiskope-d0160.firebaseapp.com",
+    databaseURL: "https://citiskope-d0160.firebaseio.com",
+    projectId: "citiskope-d0160",
+    storageBucket: "citiskope-d0160.appspot.com",
+    messagingSenderId: "800952015552"
+  },
+  citiskopeApi: {
+    baseUrl: 'https://uat-api.citiskope.com'
+  }
+};
