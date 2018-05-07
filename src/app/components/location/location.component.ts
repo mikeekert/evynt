d3516de603b038d23e1fda49632cbc6f32477aa7
Mike Ekert <mike.ekert@totalexpert.com>
2018-05-07 12:43:32 -0500

    basic modal in place

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 import * as zipcode from 'zipcodes';
 
@@ -6,30 +7,61 @@ import * as zipcode from 'zipcodes';
 export class LocationComponent implements OnInit {
   private location: zipcode.ZipCode;
 
-  constructor() {}
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
-    let latitude = localStorage.getItem('latitude');
-    let longitude = localStorage.getItem('longitude');
-
-    if (navigator.geolocation) {
-      if (latitude == null && longitude == null) {
-        navigator
-          .geolocation
-          .getCurrentPosition((position) => {
-            latitude = position.coords.latitude.toString();
-            longitude = position.coords.longitude.toString();
-
-            localStorage.setItem('latitude', latitude);
-            localStorage.setItem('longitude', longitude);
-            this.location = zipcode.lookupByCoords(latitude, longitude);
-          });
-      } else {
-        this.location = zipcode.lookupByCoords(latitude, longitude);
-      }
-    }
+    const latitude = localStorage.getItem('latitude');
+    const longitude = localStorage.getItem('longitude');
   }
+
   getLocationByZip(zip: string) {
     this.location = zipcode.lookup(zip);
+  }
+
+  changeZip() {
+    navigator
+      .geolocation
+      .getCurrentPosition((position) => {
+        const latitude = position
+          .coords
+          .latitude
+          .toString();
+        const longitude = position
+          .coords
+          .longitude
+          .toString();
+
+        localStorage.setItem('latitude', latitude);
+        localStorage.setItem('longitude', longitude);
+        this.location = zipcode.lookupByCoords(latitude, longitude);
+      });
+      console.log(this.location);
+  }
+
+  open(content) {
+    this
+      .modalService
+      .open(content, {
+        size: 'sm',
+        centered: true
+      })
+      .result
+      .then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
